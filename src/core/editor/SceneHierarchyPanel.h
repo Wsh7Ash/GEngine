ec17d43a19c7b21bdcb6a2f5747c24596dab0bec
Wsh7Ash <Wsh7Ash@users.noreply.github.com>
2026-02-27 00:32:25 +0200

    Add SceneHierarchyPanel header

---
+++ src/core/editor/SceneHierarchyPanel.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "../ecs/World.h"
+#include "../ecs/Entity.h"
+#include <memory>
+
+namespace ge {
+namespace editor {
+
+    /**
+     * @brief Panel for displaying the entity hierarchy and editing component properties.
+     */
+    class SceneHierarchyPanel
+    {
+    public:
+        SceneHierarchyPanel();
+        SceneHierarchyPanel(ecs::World& world);
+
+        void SetContext(ecs::World& world);
+
+        void OnImGuiRender();
+
+        ecs::Entity GetSelectedEntity() const { return selection_context_; }
+        void SetSelectedEntity(ecs::Entity entity) { selection_context_ = entity; }
+
+    private:
+        void DrawEntityNode(ecs::Entity entity);
+        void DrawComponents(ecs::Entity entity);
+
+    private:
+        ecs::World* context_ = nullptr;
+        ecs::Entity selection_context_; // Default constructed handle (Null)
+    };
+
+} // namespace editor
+} // namespace ge
