d996acd7f2437d41d6ed3aae77c0902d66412439
Wsh7Ash <Wsh7Ash@users.noreply.github.com>
2026-02-26 19:33:00 +0200

    Implement sprite detection in RenderSystem

--- src/core/ecs/systems/RenderSystem.cpp
+++ src/core/ecs/systems/RenderSystem.cpp
@@ -1,6 +1,7 @@
 #include "RenderSystem.h"
 #include "../../renderer/Mesh.h"
 #include "../../renderer/Shader.h"
+#include "../../renderer/Texture.h"
 #include "../../math/Mat4x4.h"
 
 namespace ge {
@@ -11,22 +12,44 @@ void RenderSystem::Render(World& world)
     for (auto const& entity : entities)
     {
         auto& transform = world.GetComponent<TransformComponent>(entity);
-        auto& meshComp  = world.GetComponent<MeshComponent>(entity);
+        
+        // 1. Handle 3D Meshes
+        if (world.HasComponent<MeshComponent>(entity))
+        {
+            auto& meshComp = world.GetComponent<MeshComponent>(entity);
+            if (meshComp.MeshPtr && meshComp.ShaderPtr)
+            {
+                meshComp.ShaderPtr->Bind();
 
-        if (!meshComp.MeshPtr || !meshComp.ShaderPtr)
-            continue;
+                Math::Mat4f model = Math::Mat4f::Translate(transform.position) * 
+                                    transform.rotation.ToMat4x4() * 
+                                    Math::Mat4f::Scale(transform.scale);
 
-        meshComp.ShaderPtr->Bind();
+                meshComp.ShaderPtr->SetMat4("u_Model", model);
+                meshComp.MeshPtr->Draw();
+            }
+        }
 
-        // Create Model Matrix
-        Math::Mat4f model = Math::Mat4f::Identity();
-        model = Math::Mat4f::Translate(transform.position) * 
-                transform.rotation.ToMat4x4() * 
-                Math::Mat4f::Scale(transform.scale);
-
-        meshComp.ShaderPtr->SetMat4("u_Model", model);
-
-        meshComp.MeshPtr->Draw();
+        // 2. Handle 2D Sprites
+        if (world.HasComponent<SpriteComponent>(entity))
+        {
+            auto& spriteComp = world.GetComponent<SpriteComponent>(entity);
+            if (spriteComp.TexturePtr)
+            {
+                spriteComp.TexturePtr->Bind(0);
+                // (Temporary) using MeshComponent's shader if available, or we'd have a default Sprite shader
+                if (world.HasComponent<MeshComponent>(entity))
+                {
+                    auto& meshComp = world.GetComponent<MeshComponent>(entity);
+                    if (meshComp.ShaderPtr)
+                    {
+                        meshComp.ShaderPtr->Bind();
+                        meshComp.ShaderPtr->SetInt("u_Texture", 0);
+                        // meshComp.ShaderPtr->SetVec4("u_Color", spriteComp.Color); // If shader supports it
+                    }
+                }
+            }
+        }
     }
 }
 
