d0eecb65a95fa01a593bf023608f50e334413d76
Wsh7Ash <Wsh7Ash@users.noreply.github.com>
2026-02-26 19:50:47 +0200

    Implement Renderer2D batching in RenderSystem

--- src/core/ecs/systems/RenderSystem.cpp
+++ src/core/ecs/systems/RenderSystem.cpp
@@ -2,6 +2,7 @@
 #include "../../renderer/Mesh.h"
 #include "../../renderer/Shader.h"
 #include "../../renderer/Texture.h"
+#include "../../renderer/Renderer2D.h"
 #include "../../math/Mat4x4.h"
 
 namespace ge {
@@ -9,18 +10,16 @@ namespace ecs {
 
 void RenderSystem::Render(World& world)
 {
+    // 1. 3D Pass (MeshComponents)
     for (auto const& entity : entities)
     {
-        auto& transform = world.GetComponent<TransformComponent>(entity);
-        
-        // 1. Handle 3D Meshes
-        if (world.HasComponent<MeshComponent>(entity))
+        if (world.HasComponent<MeshComponent>(entity) && !world.HasComponent<SpriteComponent>(entity))
         {
+            auto& transform = world.GetComponent<TransformComponent>(entity);
             auto& meshComp = world.GetComponent<MeshComponent>(entity);
             if (meshComp.MeshPtr && meshComp.ShaderPtr)
             {
                 meshComp.ShaderPtr->Bind();
-
                 Math::Mat4f model = Math::Mat4f::Translate(transform.position) * 
                                     transform.rotation.ToMat4x4() * 
                                     Math::Mat4f::Scale(transform.scale);
@@ -29,27 +28,26 @@ void RenderSystem::Render(World& world)
                 meshComp.MeshPtr->Draw();
             }
         }
+    }
 
-        // 2. Handle 2D Sprites
-        if (world.HasComponent<SpriteComponent>(entity))
+    // 2. 2D Batch Pass (SpriteComponents)
+    if (camera2D_)
+    {
+        renderer::Renderer2D::BeginScene(*camera2D_);
+        for (auto const& entity : entities)
         {
-            auto& spriteComp = world.GetComponent<SpriteComponent>(entity);
-            if (spriteComp.TexturePtr)
+            if (world.HasComponent<SpriteComponent>(entity))
             {
-                spriteComp.TexturePtr->Bind(0);
-                // (Temporary) using MeshComponent's shader if available, or we'd have a default Sprite shader
-                if (world.HasComponent<MeshComponent>(entity))
-                {
-                    auto& meshComp = world.GetComponent<MeshComponent>(entity);
-                    if (meshComp.ShaderPtr)
-                    {
-                        meshComp.ShaderPtr->Bind();
-                        meshComp.ShaderPtr->SetInt("u_Texture", 0);
-                        // meshComp.ShaderPtr->SetVec4("u_Color", spriteComp.Color); // If shader supports it
-                    }
-                }
+                auto& transform = world.GetComponent<TransformComponent>(entity);
+                auto& spriteComp = world.GetComponent<SpriteComponent>(entity);
+                
+                if (spriteComp.TexturePtr)
+                    renderer::Renderer2D::DrawQuad(transform.position, { transform.scale.x, transform.scale.y }, spriteComp.TexturePtr, spriteComp.Color);
+                else
+                    renderer::Renderer2D::DrawQuad(transform.position, { transform.scale.x, transform.scale.y }, spriteComp.Color);
             }
         }
+        renderer::Renderer2D::EndScene();
     }
 }
 
