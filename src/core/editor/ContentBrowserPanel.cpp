7b635930ccbda974178940f081fc349fcc305d1b
Wsh7Ash <Wsh7Ash@users.noreply.github.com>
2026-02-27 20:06:33 +0200

    Added file selection basic

--- src/core/editor/ContentBrowserPanel.cpp
+++ src/core/editor/ContentBrowserPanel.cpp
@@ -3,18 +3,37 @@
 
 namespace ge{
     namespace editor{
-        static const std::filesystem::path s_AssetsDirectory = "path/to/assets";
+        static const std::filesystem::path s_AssetsDirectory = "../assets";
         
         ContentBrowserPanel::ContentBrowserPanel() : cur_dir_(s_AssetsDirectory){
 
         }
 
+        // TODO: Add file selection
         void ContentBrowserPanel::OnImGuiRender(){
+            ImGui::Begin("Content Browser");
             if(cur_dir_ != std::filesystem::path(s_AssetsDirectory)){
                 if(ImGui::Button("<-Back")){
                     cur_dir_ = cur_dir_.parent_path();
                 }
             }
+
+            for(auto& directoryEntity : std::filesystem::directory_iterator(cur_dir_)){
+                const auto& path = directoryEntity.path();
+                auto relativePath = std::filesystem::relative(path, s_AssetsDirectory);
+                std::string filenameString = path.filename().string();
+                if(directoryEntity.is_directory()){
+                    if(ImGui::Button(filenameString.c_str())){
+                        cur_dir_ /= path.filename();
+                    }
+                }else{
+                    if(ImGui::Button(filenameString.c_str())){
+                        // TODO: Open file
+                    }
+                }
+            }
+
+            ImGui::End();
         }
     }
 }
