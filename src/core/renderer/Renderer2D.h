c446284f091ddd07f79070b927170577b5bdd837
Wsh7Ash <Wsh7Ash@users.noreply.github.com>
2026-02-28 23:43:05 +0200

    feat: Implement OpenGL 2D renderer with framebuffer support and sprite batching, updating scene entity transforms and colors.

--- src/core/renderer/Renderer2D.h
+++ src/core/renderer/Renderer2D.h
@@ -1,47 +1,54 @@
 #pragma once
 
-#include "OrthographicCamera.h"
-#include "Texture.h"
-#include "Shader.h"
 #include "../math/VecTypes.h"
+#include "OrthographicCamera.h"
+#include "Shader.h"
+#include "Texture.h"
 #include <memory>
+
 
 namespace ge {
 namespace renderer {
 
-    struct Renderer2DStatistics
-    {
-        uint32_t DrawCalls = 0;
-        uint32_t QuadCount = 0;
+struct Renderer2DStatistics {
+  uint32_t DrawCalls = 0;
+  uint32_t QuadCount = 0;
 
-        uint32_t GetTotalVertexCount() const { return QuadCount * 4; }
-        uint32_t GetTotalIndexCount() const { return QuadCount * 6; }
-    };
+  uint32_t GetTotalVertexCount() const { return QuadCount * 4; }
+  uint32_t GetTotalIndexCount() const { return QuadCount * 6; }
+};
 
-    class Renderer2D
-    {
-    public:
-        static void Init();
-        static void Shutdown();
+class Renderer2D {
+public:
+  static void Init();
+  static void Shutdown();
 
-        static void BeginScene(const OrthographicCamera& camera);
-        static void EndScene();
-        static void Flush();
+  static void BeginScene(const OrthographicCamera &camera);
+  static void EndScene();
+  static void Flush();
 
-        // Primitives
-        static void DrawQuad(const Math::Vec2f& position, const Math::Vec2f& size, const Math::Vec4f& color);
-        static void DrawQuad(const Math::Vec3f& position, const Math::Vec2f& size, const Math::Vec4f& color);
-        static void DrawQuad(const Math::Vec2f& position, const Math::Vec2f& size, const std::shared_ptr<Texture>& texture, const Math::Vec4f& tint = { 1.0f, 1.0f, 1.0f, 1.0f });
-        static void DrawQuad(const Math::Vec3f& position, const Math::Vec2f& size, const std::shared_ptr<Texture>& texture, const Math::Vec4f& tint = { 1.0f, 1.0f, 1.0f, 1.0f });
+  // Primitives
+  static void DrawQuad(const Math::Vec2f &position, const Math::Vec2f &size,
+                       const Math::Vec4f &color, int entityID = -1);
+  static void DrawQuad(const Math::Vec3f &position, const Math::Vec2f &size,
+                       const Math::Vec4f &color, int entityID = -1);
+  static void DrawQuad(const Math::Vec2f &position, const Math::Vec2f &size,
+                       const std::shared_ptr<Texture> &texture,
+                       const Math::Vec4f &tint = {1.0f, 1.0f, 1.0f, 1.0f},
+                       int entityID = -1);
+  static void DrawQuad(const Math::Vec3f &position, const Math::Vec2f &size,
+                       const std::shared_ptr<Texture> &texture,
+                       const Math::Vec4f &tint = {1.0f, 1.0f, 1.0f, 1.0f},
+                       int entityID = -1);
 
-        // Stats
-        static void ResetStats();
-        static Renderer2DStatistics GetStats();
+  // Stats
+  static void ResetStats();
+  static Renderer2DStatistics GetStats();
 
-    private:
-        static void StartBatch();
-        static void NextBatch();
-    };
+private:
+  static void StartBatch();
+  static void NextBatch();
+};
 
 } // namespace renderer
 } // namespace ge
