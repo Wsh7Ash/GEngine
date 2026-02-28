5e2fb65acf3836ab2fa7aae43c6357aa03208541
Wsh7Ash <Wsh7Ash@users.noreply.github.com>
2026-02-28 23:34:22 +0200

    feat: Implement 2D renderer with sprite batching, framebuffer, and editor viewport panel.

--- src/core/editor/ViewportPanel.h
+++ src/core/editor/ViewportPanel.h
@@ -1,31 +1,42 @@
 #pragma once
 
-#include "../renderer/Framebuffer.h"
 #include "../math/VecTypes.h"
+#include "../renderer/Framebuffer.h"
+#include <imgui.h>
 #include <memory>
 
+
 namespace ge {
+namespace ecs {
+class World;
+}
+
 namespace editor {
 
-    /**
-     * @brief Panel that displays the rendered Framebuffer texture.
-     */
-    class ViewportPanel
-    {
-    public:
-        ViewportPanel();
-        ~ViewportPanel() = default;
+/**
+ * @brief Panel that displays the rendered Framebuffer texture.
+ */
+class ViewportPanel {
+public:
+  ViewportPanel();
+  ~ViewportPanel() = default;
 
-        void OnImGuiRender();
+  void OnImGuiRender();
 
-        std::shared_ptr<renderer::Framebuffer> GetFramebuffer() { return framebuffer_; }
+  void SetContext(ecs::World &world) { sceneContext_ = &world; }
+  std::shared_ptr<renderer::Framebuffer> GetFramebuffer() {
+    return framebuffer_;
+  }
 
-    private:
-        std::shared_ptr<renderer::Framebuffer> framebuffer_;
-        bool isFocused_ = false;
-        bool isHovered_ = false;
-        Math::Vec2f viewportSize_ = { 0, 0 };
-    };
+private:
+  std::shared_ptr<renderer::Framebuffer> framebuffer_;
+  ecs::World *sceneContext_ = nullptr;
+
+  bool isFocused_ = false;
+  bool isHovered_ = false;
+  Math::Vec2f viewportSize_ = {0, 0};
+  Math::Vec2f viewportBounds_[2];
+};
 
 } // namespace editor
 } // namespace ge
