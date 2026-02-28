84820017fd417cb56f29bd167dba6079ea80e64d
Wsh7Ash <Wsh7Ash@users.noreply.github.com>
2026-02-28 23:50:06 +0200

    feat: Add OpenGL framebuffer and mesh implementations, along with sprite batching shaders.

--- src/core/renderer/opengl/OpenGLFramebuffer.h
+++ src/core/renderer/opengl/OpenGLFramebuffer.h
@@ -5,28 +5,36 @@
 namespace ge {
 namespace renderer {
 
-    class OpenGLFramebuffer : public Framebuffer
-    {
-    public:
-        OpenGLFramebuffer(const FramebufferSpecification& spec);
-        virtual ~OpenGLFramebuffer();
+class OpenGLFramebuffer : public Framebuffer {
+public:
+  OpenGLFramebuffer(const FramebufferSpecification &spec);
+  virtual ~OpenGLFramebuffer();
 
-        void Invalidate();
+  void Invalidate();
 
-        virtual void Bind() override;
-        virtual void Unbind() override;
+  virtual void Bind() override;
+  virtual void Unbind() override;
 
-        virtual void Resize(uint32_t width, uint32_t height) override;
+  virtual void Resize(uint32_t width, uint32_t height) override;
 
-        virtual uint32_t GetColorAttachmentRendererID() const override { return colorAttachment_; }
-        virtual const FramebufferSpecification& GetSpecification() const override { return spec_; }
+  virtual uint32_t GetColorAttachmentRendererID() const override {
+    return colorAttachment_;
+  }
+  virtual uint32_t GetEntityAttachmentRendererID() const override {
+    return entityAttachment_;
+  }
+  virtual int ReadPixel(uint32_t attachmentIndex, int x, int y) override;
+  virtual const FramebufferSpecification &GetSpecification() const override {
+    return spec_;
+  }
 
-    private:
-        uint32_t rendererID_ = 0;
-        uint32_t colorAttachment_ = 0;
-        uint32_t depthAttachment_ = 0;
-        FramebufferSpecification spec_;
-    };
+private:
+  uint32_t rendererID_ = 0;
+  uint32_t colorAttachment_ = 0;
+  uint32_t entityAttachment_ = 0;
+  uint32_t depthAttachment_ = 0;
+  FramebufferSpecification spec_;
+};
 
 } // namespace renderer
 } // namespace ge
