f59e67439f2267f82a95ad648027daaf5d9bb879
Wsh7Ash <Wsh7Ash@users.noreply.github.com>
2026-03-05 19:28:47 +0200

    Phase 21 (Step 7/10): Implemented Framebuffer MSAA support with blit resolve.

--- src/core/renderer/opengl/OpenGLFramebuffer.cpp
+++ src/core/renderer/opengl/OpenGLFramebuffer.cpp
@@ -16,6 +16,12 @@ OpenGLFramebuffer::~OpenGLFramebuffer() {
   glDeleteTextures(1, &colorAttachment_);
   glDeleteTextures(1, &entityAttachment_);
   glDeleteTextures(1, &depthAttachment_);
+
+  if (resolvedRendererID_) {
+    glDeleteFramebuffers(1, &resolvedRendererID_);
+    glDeleteTextures(1, &resolvedColorAttachment_);
+    glDeleteTextures(1, &resolvedEntityAttachment_);
+  }
 }
 
 void OpenGLFramebuffer::Invalidate() {
@@ -24,40 +30,101 @@ void OpenGLFramebuffer::Invalidate() {
     glDeleteTextures(1, &colorAttachment_);
     glDeleteTextures(1, &entityAttachment_);
     glDeleteTextures(1, &depthAttachment_);
+
+    if (resolvedRendererID_) {
+      glDeleteFramebuffers(1, &resolvedRendererID_);
+      glDeleteTextures(1, &resolvedColorAttachment_);
+      glDeleteTextures(1, &resolvedEntityAttachment_);
+      resolvedRendererID_ = 0;
+    }
   }
+
+  bool multisampled = spec_.Samples > 1;
 
   glCreateFramebuffers(1, &rendererID_);
   glBindFramebuffer(GL_FRAMEBUFFER, rendererID_);
 
   // Color Attachment
-  glCreateTextures(GL_TEXTURE_2D, 1, &colorAttachment_);
-  glBindTexture(GL_TEXTURE_2D, colorAttachment_);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, spec_.Width, spec_.Height, 0,
-               GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
-                         colorAttachment_, 0);
+  if (multisampled) {
+    glCreateTextures(GL_TEXTURE_2D_MULTISAMPLE, 1, &colorAttachment_);
+    glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, colorAttachment_);
+    glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, spec_.Samples, GL_RGBA8,
+                            spec_.Width, spec_.Height, GL_FALSE);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+                           GL_TEXTURE_2D_MULTISAMPLE, colorAttachment_, 0);
+  } else {
+    glCreateTextures(GL_TEXTURE_2D, 1, &colorAttachment_);
+    glBindTexture(GL_TEXTURE_2D, colorAttachment_);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, spec_.Width, spec_.Height, 0,
+                 GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
+                           colorAttachment_, 0);
+  }
 
   // Entity ID Attachment
-  glCreateTextures(GL_TEXTURE_2D, 1, &entityAttachment_);
-  glBindTexture(GL_TEXTURE_2D, entityAttachment_);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_R32I, spec_.Width, spec_.Height, 0,
-               GL_RED_INTEGER, GL_INT, nullptr);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D,
-                         entityAttachment_, 0);
+  if (multisampled) {
+    glCreateTextures(GL_TEXTURE_2D_MULTISAMPLE, 1, &entityAttachment_);
+    glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, entityAttachment_);
+    glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, spec_.Samples, GL_R32I,
+                            spec_.Width, spec_.Height, GL_FALSE);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1,
+                           GL_TEXTURE_2D_MULTISAMPLE, entityAttachment_, 0);
+  } else {
+    glCreateTextures(GL_TEXTURE_2D, 1, &entityAttachment_);
+    glBindTexture(GL_TEXTURE_2D, entityAttachment_);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_R32I, spec_.Width, spec_.Height, 0,
+                 GL_RED_INTEGER, GL_INT, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D,
+                           entityAttachment_, 0);
+  }
 
   // Depth Attachment
-  glCreateTextures(GL_TEXTURE_2D, 1, &depthAttachment_);
-  glBindTexture(GL_TEXTURE_2D, depthAttachment_);
-  glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH24_STENCIL8, spec_.Width,
-                 spec_.Height);
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT,
-                         GL_TEXTURE_2D, depthAttachment_, 0);
+  if (multisampled) {
+    glCreateTextures(GL_TEXTURE_2D_MULTISAMPLE, 1, &depthAttachment_);
+    glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, depthAttachment_);
+    glTexStorage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, spec_.Samples,
+                              GL_DEPTH24_STENCIL8, spec_.Width, spec_.Height,
+                              GL_FALSE);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT,
+                           GL_TEXTURE_2D_MULTISAMPLE, depthAttachment_, 0);
+  } else {
+    glCreateTextures(GL_TEXTURE_2D, 1, &depthAttachment_);
+    glBindTexture(GL_TEXTURE_2D, depthAttachment_);
+    glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH24_STENCIL8, spec_.Width,
+                   spec_.Height);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT,
+                           GL_TEXTURE_2D, depthAttachment_, 0);
+  }
+
+  // Create Resolve Framebuffer
+  if (multisampled) {
+    glCreateFramebuffers(1, &resolvedRendererID_);
+    glBindFramebuffer(GL_FRAMEBUFFER, resolvedRendererID_);
+
+    glCreateTextures(GL_TEXTURE_2D, 1, &resolvedColorAttachment_);
+    glBindTexture(GL_TEXTURE_2D, resolvedColorAttachment_);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, spec_.Width, spec_.Height, 0,
+                 GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D,
+                           resolvedColorAttachment_, 0);
+
+    glCreateTextures(GL_TEXTURE_2D, 1, &resolvedEntityAttachment_);
+    glBindTexture(GL_TEXTURE_2D, resolvedEntityAttachment_);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_R32I, spec_.Width, spec_.Height, 0,
+                 GL_RED_INTEGER, GL_INT, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D,
+                           resolvedEntityAttachment_, 0);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, rendererID_);
+  }
 
   // Map drawing explicitly to both attachments
   static const GLenum buffers[2] = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1};
@@ -76,6 +143,12 @@ void OpenGLFramebuffer::ClearAttachment(uint32_t attachmentIndex, int value) {
 
 int OpenGLFramebuffer::ReadPixel(uint32_t attachmentIndex, int x, int y) {
   GE_ASSERT(attachmentIndex < 2, "Invalid attachment index!");
+  if (spec_.Samples > 1) {
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, resolvedRendererID_);
+  } else {
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, rendererID_);
+  }
+
   glReadBuffer(GL_COLOR_ATTACHMENT0 + attachmentIndex);
   int pixelData = -1;
   glReadPixels(x, y, 1, 1, GL_RED_INTEGER, GL_INT, &pixelData);
@@ -87,7 +160,25 @@ void OpenGLFramebuffer::Bind() {
   glViewport(0, 0, spec_.Width, spec_.Height);
 }
 
-void OpenGLFramebuffer::Unbind() { glBindFramebuffer(GL_FRAMEBUFFER, 0); }
+void OpenGLFramebuffer::Unbind() {
+  if (spec_.Samples > 1) {
+    glBindFramebuffer(GL_READ_FRAMEBUFFER, rendererID_);
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, resolvedRendererID_);
+
+    // Resolve Color
+    glReadBuffer(GL_COLOR_ATTACHMENT0);
+    glDrawBuffer(GL_COLOR_ATTACHMENT0);
+    glBlitFramebuffer(0, 0, spec_.Width, spec_.Height, 0, 0, spec_.Width,
+                      spec_.Height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+
+    // Resolve Entity ID
+    glReadBuffer(GL_COLOR_ATTACHMENT1);
+    glDrawBuffer(GL_COLOR_ATTACHMENT1);
+    glBlitFramebuffer(0, 0, spec_.Width, spec_.Height, 0, 0, spec_.Width,
+                      spec_.Height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+  }
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
 
 void OpenGLFramebuffer::Resize(uint32_t width, uint32_t height) {
   spec_.Width = width;
--- src/core/renderer/opengl/OpenGLFramebuffer.h
+++ src/core/renderer/opengl/OpenGLFramebuffer.h
@@ -18,10 +18,10 @@ public:
   virtual void Resize(uint32_t width, uint32_t height) override;
 
   virtual uint32_t GetColorAttachmentRendererID() const override {
-    return colorAttachment_;
+    return spec_.Samples > 1 ? resolvedColorAttachment_ : colorAttachment_;
   }
   virtual uint32_t GetEntityAttachmentRendererID() const override {
-    return entityAttachment_;
+    return spec_.Samples > 1 ? resolvedEntityAttachment_ : entityAttachment_;
   }
   virtual void ClearAttachment(uint32_t attachmentIndex, int value) override;
   virtual int ReadPixel(uint32_t attachmentIndex, int x, int y) override;
@@ -34,6 +34,10 @@ private:
   uint32_t colorAttachment_ = 0;
   uint32_t entityAttachment_ = 0;
   uint32_t depthAttachment_ = 0;
+
+  uint32_t resolvedRendererID_ = 0;
+  uint32_t resolvedColorAttachment_ = 0;
+  uint32_t resolvedEntityAttachment_ = 0;
   FramebufferSpecification spec_;
 };
 
