3442427c375671fa8c50417e3904ab3a7ae2a96c
Wsh7Ash <Wsh7Ash@users.noreply.github.com>
2026-03-07 01:10:20 +0200

    updated headers proc

--- src/core/ge_core.h
+++ src/core/ge_core.h
@@ -12,57 +12,64 @@
 #include "memory/allocator.h"
 
 // Math
-#include "math/mathUtils.h"
-#include "math/VecTypes.h"
 #include "math/Mat4x4.h"
+#include "math/VecTypes.h"
+#include "math/mathUtils.h"
 #include "math/quaternion.h"
 
+
 // Debug
-#include "debug/log.h"
 #include "debug/assert.h"
+#include "debug/log.h"
+
 
 // Containers
 #include "containers/dynamic_array.h"
-#include "containers/hash_map.h"
 #include "containers/handle.h"
+#include "containers/hash_map.h"
+
 
 // Platform
-#include "platform/platform.h"
-#include "platform/Window.h"
-#include "platform/Input.h"
 #include "platform/GLFWInput.h"
 #include "platform/ImGuiLayer.h"
+#include "platform/Input.h"
+#include "platform/Window.h"
+#include "platform/platform.h"
+
 
 // ECS
-#include "ecs/Entity.h"
-#include "ecs/ComponentRegistry.h"
-#include "ecs/EntityManager.h"
 #include "ecs/ComponentArray.h"
-#include "ecs/World.h"
+#include "ecs/ComponentRegistry.h"
+#include "ecs/Entity.h"
+#include "ecs/EntityManager.h"
+#include "ecs/ScriptableEntity.h"
 #include "ecs/System.h"
 #include "ecs/SystemManager.h"
+#include "ecs/World.h"
+#include "ecs/components/MeshComponent.h"
+#include "ecs/components/NativeScriptComponent.h"
+#include "ecs/components/SpriteComponent.h"
+#include "ecs/components/TagComponent.h"
 #include "ecs/components/TransformComponent.h"
 #include "ecs/components/VelocityComponent.h"
-#include "ecs/components/TagComponent.h"
-#include "ecs/components/MeshComponent.h"
-#include "ecs/components/SpriteComponent.h"
-#include "ecs/components/NativeScriptComponent.h"
 #include "ecs/systems/RenderSystem.h"
 #include "ecs/systems/ScriptSystem.h"
-#include "ecs/ScriptableEntity.h"
+
 
 // Renderer
-#include "renderer/RendererAPI.h"
 #include "renderer/GraphicsContext.h"
-#include "renderer/Shader.h"
 #include "renderer/Mesh.h"
-#include "renderer/Texture.h"
 #include "renderer/OrthographicCamera.h"
 #include "renderer/Renderer2D.h"
+#include "renderer/RendererAPI.h"
+#include "renderer/Shader.h"
+#include "renderer/Texture.h"
+
 
 // Editor
 #include "editor/EditorToolbar.h"
 #include "editor/SceneHierarchyPanel.h"
+#include "editor/ViewportPanel.h"
 
 // Scene
 #include "scene/SceneSerializer.h"
