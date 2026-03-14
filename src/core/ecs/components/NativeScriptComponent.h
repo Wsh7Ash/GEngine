9fad64daf3cda06932225eafbf071b2b29425e9a
Wsh7Ash <Wsh7Ash@users.noreply.github.com>
2026-03-14 17:36:31 +0200

    Scripting name

--- src/core/ecs/components/NativeScriptComponent.h
+++ src/core/ecs/components/NativeScriptComponent.h
@@ -11,15 +11,43 @@ namespace ecs {
  */
 struct NativeScriptComponent {
   ScriptableEntity *instance = nullptr;
+  std::string ScriptName = "";
 
   // Function pointers for instantiation and destruction
   std::function<ScriptableEntity *()> InstantiateScript;
   std::function<void(NativeScriptComponent *)> DestroyScript;
 
+  // Static registry to allow serializing script bindings by name
+  using BindFunc = std::function<void(NativeScriptComponent *)>;
+  static std::unordered_map<std::string, BindFunc> &GetRegistry() {
+    static std::unordered_map<std::string, BindFunc> registry;
+    return registry;
+  }
+
+  template <typename T> static void Register(const std::string &name) {
+    GetRegistry()[name] = [](NativeScriptComponent *nsc) { nsc->Bind<T>(); };
+  }
+
+  static void BindByName(NativeScriptComponent *nsc, const std::string &name) {
+    GE_LOG_INFO("BindByName called for script: %s", name.c_str());
+    fflush(stdout);
+    if (GetRegistry().count(name)) {
+      GE_LOG_INFO("Binding found in registry for: %s", name.c_str());
+      fflush(stdout);
+      GetRegistry()[name](nsc);
+      nsc->ScriptName = name;
+      GE_LOG_INFO("Successfully bound script: %s", name.c_str());
+      fflush(stdout);
+    } else {
+      GE_LOG_WARNING("BindByName could not find script: %s", name.c_str());
+      fflush(stdout);
+    }
+  }
+
   NativeScriptComponent() = default;
 
   NativeScriptComponent(NativeScriptComponent &&other) noexcept
-      : instance(other.instance),
+      : instance(other.instance), ScriptName(std::move(other.ScriptName)),
         InstantiateScript(std::move(other.InstantiateScript)),
         DestroyScript(std::move(other.DestroyScript)) {
     other.instance = nullptr;
@@ -32,6 +60,7 @@ struct NativeScriptComponent {
         DestroyScript(this);
 
       instance = other.instance;
+      ScriptName = std::move(other.ScriptName);
       InstantiateScript = std::move(other.InstantiateScript);
       DestroyScript = std::move(other.DestroyScript);
 
