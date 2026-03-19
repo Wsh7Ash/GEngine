9d7ced3c0df4202c1f8058f04b604f7068631ccc
Wsh7Ash <Wsh7Ash@users.noreply.github.com>
2026-03-19 23:40:55 +0200

    Asset handler

---
+++ src/core/assets/Asset.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "../uuid/UUID.h"
+#include <string>
+
+namespace ge {
+namespace assets {
+
+    using AssetHandle = ge::UUID;
+
+    enum class AssetType : uint16_t
+    {
+        None = 0,
+        Scene,
+        Texture,
+        Mesh,
+        Shader,
+        Material,
+        Audio,
+        Font,
+        Script
+    };
+
+    /**
+     * @brief Base class for all engine assets.
+     */
+    class Asset
+    {
+    public:
+        AssetHandle Handle;
+
+        virtual ~Asset() = default;
+        virtual AssetType GetType() const = 0;
+    };
+
+} // namespace assets
+} // namespace ge
